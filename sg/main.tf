83313ba41b769a55db8c0e7fc7c9be70792e923f
asmitha <140152177+asmitha456@users.noreply.github.com>
2025-11-19 15:19:03 +0530

    Update main.tf

--- sg/main.tf
+++ sg/main.tf
@@ -2,12 +2,12 @@
 resource "aws_security_group" "terraform_sg" {
     name = "allow ssh"
     description = "sg for to allow ssh"
-    vpc_id = aws_vpc.my_vpc.id 
+#    vpc_id = aws_vpc.my_vpc.id 
     ingress {
         from_port = 22
         to_port = 22
         protocol = "tcp"
-        cidr_block = ["0.0.0.0/0"]
+        cidr_blocks = ["0.0.0.0/0"]
     }
 
     egress {
