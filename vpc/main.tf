f620fc6a2114e4f3bfeaf0e4c98d2ed464702fff
asmitha <140152177+asmitha456@users.noreply.github.com>
2025-11-19 13:45:15 +0530

    Create main.tf

---
+++ vpc/main.tf
@@ -0,0 +1,59 @@
+#create an vpc
+resource "aws_vpc" "my_vpc" {
+    cidr_block = "10.0.0.0./16"
+    instance_tenancy = "default"
+    enable_dns_support = true
+    enable_dns_hostnames = true
+    taggs = {
+        Name = "My_VPC"
+    }
+}
+
+#create subnets
+resource "aws_subnet" "public_subnet" {
+    vpc_id = aws_vpc.my_vpc.vpc_id
+    cidr_block = "10.0.1.0/24"
+    map_public_ip_on_launch = true
+    availability_zone = var.zone1
+    tags = {
+        Name = "Public_Subnet"
+    }
+}
+resource "aws_subnet" "private_subnet" {
+    vpc_id = aws_vpc.my_vpc.vpc_id
+    cidr_block = "10.0.2.0/24"
+    map_public_ip_on_launch = true
+    availability_zone = var.zone2
+    tags = {
+        Name = "Public_Subnet"
+    }
+}
+
+#create an internet gateway
+resource "aws_internet_gateway" "igw" {
+    vpc_id = aws_vpc.my_vpc.vpc_id
+    tags = {
+        Name = "IGW"
+    }
+}
+
+#create public route and associate the public subnets with internet gateway
+resource "aws_route_table" "public_rt" {
+    vpc_id = aws_vpc.my_vpc.id 
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.igw.id
+    }
+    tags = {
+        Name = "Public-RT"
+    }
+}
+ resource "aws_route_table_association" "public_subnet_1a" {
+    subnet_id = aws_subnet.public_subnet.id
+    route_table_id = aws_route_table.public_rt.id
+}
+
+#create nat gateway
+#create private route and associate private subntes with nat gateway
+
